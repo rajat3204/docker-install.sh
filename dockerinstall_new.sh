837f9f780f4855c0e845930b6d32aab169e07137
shivamgrover <53823834+shivamgrover@users.noreply.github.com>
2023-09-07 18:44:44 +0530

    Updated docker compose version

--- dockerinstall_new.sh
+++ dockerinstall_new.sh
@@ -69,9 +69,13 @@ echo -ne 'Progress =============>            (66%)\r'
 sleep 0.3
 echo -ne "Progress ========================> (100%)\r"
 echo -ne '\n'
-sudo curl -sL "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
-sudo chmod +x /usr/local/bin/docker-compose &> /dev/null
+#OLD METHOD
+#sudo curl -sL "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
+#sudo chmod +x /usr/local/bin/docker-compose &> /dev/null
+sudo apt-get install docker-compose-plugin -y
 sleep 0.3
 echo -e "\n $(tput setaf 1) $(tput bold)DOCKER & DOCKER-COMPOSE Installed Successfuly!!!$(tput sgr0) $(tput sgr 0)\n"
 #sudo su ubuntu
 sudo su $(whoami)
+sudo docker -v
+sudo docker compose --verison
